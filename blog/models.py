a79f4fe6c7e212c3c836208371af70f10cc277ef
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2021-02-01 12:05:35 +0100

    add auto slug create

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+from django.db.models.signals import pre_save
 
 STATUS = (
     (0, "Draft"),
@@ -9,7 +11,7 @@ STATUS = (
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=250, unique=True, blank=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now=True)
@@ -22,3 +24,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        slug = slugify(instance.title)
+
+        has_slug = Post.objects.filter(slug=slug).exists()
+        count = 1
+
+        while has_slug:
+            count += 1
+            slug = slugify(instance.title) + '-' + str(count)
+            has_slug = Post.objects.filter(slug=slug).exists()
+
+        instance.slug = slug
+
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
