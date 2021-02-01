a5ff6925dc58f058ea01f693dfa08b9d95a2c9c4
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2021-02-01 13:10:53 +0100

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 
+
+app_name = 'blog'
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
