2f041227ae904a640736ffd4d65d3bbbc4ddf627
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2021-02-01 12:06:31 +0100

    add Comment model

--- blog/models.py
+++ blog/models.py
@@ -26,6 +26,22 @@ class Post(models.Model):
         return self.title
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
+
+
 def pre_save_post_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         slug = slugify(instance.title)
