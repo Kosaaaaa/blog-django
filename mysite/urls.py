d14d85d14639391ef1ea423fbd042b8433132680
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2021-02-07 00:14:13 +0100

    add about view

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('', include('blog.urls', namespace='blog')),
-
 ]
 
 if settings.DEBUG:
