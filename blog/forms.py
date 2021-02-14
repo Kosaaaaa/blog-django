33bf07c170224b26d33d084cea0e6c70618aa9e2
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2021-02-14 12:56:28 +0100

    add labels to CommentForm

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,7 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
+        labels = {
+            'name': 'Imię',
+            'body': 'Treść',
+        }
